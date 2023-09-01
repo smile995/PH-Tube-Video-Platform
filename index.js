c6afc29b5fe544617a117c7103d779adc1c40b5c
smile995 <ah.smile995@gmai.com>
2023-09-02 00:24:43 +0600

    second commit

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// console.log('whats your name');
+const PHTube=async()=>{
+    const response= await fetch('https://openapi.programming-hero.com/api/videos/categories');
+    const data= await response.json();
+    const categories=data.data;
+    categories.forEach(categiry => {
+        console.log(categiry.category);
+    });
+    // console.log(categories);
+}
+PHTube();
