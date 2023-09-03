4dc8912c00ea686bdf88faa8e00ea47780a3a9df
smile995 <ah.smile995@gmai.com>
2023-09-03 15:51:01 +0600

    forth commit

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
-const PHTube=async()=>{
-    const response= await fetch('https://openapi.programming-hero.com/api/videos/categories');
-    const data= await response.json();
-    const categories=data.data;
-    categories.forEach(categiry => {
-        console.log(categiry.category);
+const PHTube = async () => {
+    const response = await fetch('https://openapi.programming-hero.com/api/videos/categories');
+    const data = await response.json();
+    const categories = data.data;
+    const buttonContainer = document.getElementById('button_container');
+
+    categories.forEach((category) => {
+        const newDiv = document.createElement('div')
+        newDiv.innerHTML = `
+        <button onclick="videoDisplay('${category.category_id}')">${category.category}</button>
+        `;
+        buttonContainer.appendChild(newDiv)
+        // console.log(category);
     });
-    // console.log(categories);
+
 }
+const videoDisplay = async(categoryid) => {
+    const response=await fetch (`https://openapi.programming-hero.com/api/videos/category/${categoryid}`);
+    const data=await response.json();
+    const videos=data.data
+    // console.log(videos);
+    videos.forEach(video=>{
+        console.log(video);
+    })
+
+    // console.log(categoryid);
+};
+
+
+
+
 PHTube();
